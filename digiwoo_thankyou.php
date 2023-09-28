bb3eec8a0b567175d6425087ad48ec0a646d255e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-28 21:20:39 +0800

    Initial commit

---
+++ digiwoo_thankyou.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name: Digiwoo Thank You Page Woocommerce
+ * Description: Custom Thank You page for WooCommerce
+ * Version: 1.0.1
+ * Author: Ardika JM-Consulting
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!class_exists('Digiwoo_ThankYou')) {
+
+    class Digiwoo_ThankYou {
+
+        public function __construct() {
+            add_action('plugins_loaded', array($this, 'init'));
+            add_action('woocommerce_thankyou', array($this, 'custom_redirect'));
+            add_filter('woocommerce_get_settings_pages', array($this, 'add_settings_page'));
+        }
+
+        public function init() {
+            if (!class_exists('WC_Payment_Gateway')) {
+                return;
+            }
+            
+            include_once 'settings.php';
+        }
+
+        public function custom_redirect($order_id) {
+            $enabled = get_option('digiwoo_thankyou_enabled');
+            $thank_you_page = get_option('digiwoo_thankyou_page');
+            
+            if ($enabled === 'yes' && $thank_you_page) {
+                $redirect_url = get_permalink($thank_you_page);
+                if ($redirect_url) {
+                    wp_safe_redirect($redirect_url);
+                    exit;
+                }
+            }
+        }
+
+        public function add_settings_page($settings) {
+            $settings[] = include 'settings.php';
+            return $settings;
+        }
+    }
+
+    new Digiwoo_ThankYou();
+}
+
